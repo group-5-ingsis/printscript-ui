d9a0dbc9094e8277f82aedf795d9adb8222dc724
husseymarcos <mhussey@mail.austral.edu.ar>
2024-11-10 20:28:40 -0300

    testing endpoint

--- src/utils/SnippetOperationsImpl.ts
+++ src/utils/SnippetOperationsImpl.ts
@@ -17,7 +17,7 @@ export class SnippetOperationsImpl implements SnippetOperations{
   async getFormatRules(): Promise<Rule[]> {
     const token = await this.getToken();
     try {
-      const response = await fetch(`/snippets/v1/snippet/format/rules`, {
+      const response = await fetch(`/snippet/format/rules`, {
         method: 'GET',
         headers: {
           'Authorization': `Bearer ${token}`,
