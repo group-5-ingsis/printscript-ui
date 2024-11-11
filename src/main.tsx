545e2e3c8eb26172ef31d00e1755e6a614b0bd76
husseymarcos <mhussey@mail.austral.edu.ar>
2024-11-11 10:10:11 -0300

    test

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import {Auth0Provider} from "@auth0/auth0-react";
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Auth0Provider
-            domain={import.meta.env.VITE_AUTH0_DOMAIN ?? ""}
+            domain={import.meta.env.VITE_AUTH0_DOMAIN ?? "hellooo"}
             clientId={import.meta.env.VITE_AUTH0_CLIENT_ID ?? ""}
             authorizationParams={{
                 redirect_uri: window.location.origin,
