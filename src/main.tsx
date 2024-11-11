7b86acd2acff02889d2d340b74f679ff6c3e0f4b
husseymarcos <mhussey@mail.austral.edu.ar>
2024-11-11 14:25:43 -0300

    test 2

--- src/main.tsx
+++ src/main.tsx
@@ -9,12 +9,11 @@ import {Auth0Provider} from "@auth0/auth0-react";
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Auth0Provider
-            domain={import.meta.env.VITE_AUTH0_DOMAIN ?? "hellooo"}
+            domain={import.meta.env.VITE_AUTH0_DOMAIN ?? "hellooouauauaua"}
             clientId={import.meta.env.VITE_AUTH0_CLIENT_ID ?? ""}
             authorizationParams={{
                 redirect_uri: window.location.origin,
                 audience: import.meta.env.VITE_AUTH0_AUDIENCE ,
-                scope: "read:snippets write:snippets",
             }}
         >
             <PaginationProvider>
