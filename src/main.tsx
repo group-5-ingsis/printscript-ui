036e54727136294d16f20f5f49b121dc52437c04
husseymarcos <mhussey@mail.austral.edu.ar>
2024-11-11 14:53:43 -0300

    test console logs

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import App from './App.tsx'
-import './index.css'
-import {createRoot} from "react-dom/client";
-import {PaginationProvider} from "./contexts/paginationProvider.tsx";
-import {SnackbarProvider} from "./contexts/snackbarProvider.tsx";
-import {Auth0Provider} from "@auth0/auth0-react";
+import App from './App.tsx';
+import './index.css';
+import { createRoot } from "react-dom/client";
+import { PaginationProvider } from "./contexts/paginationProvider.tsx";
+import { SnackbarProvider } from "./contexts/snackbarProvider.tsx";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+console.log('VITE_AUTH0_DOMAIN:', import.meta.env.VITE_AUTH0_DOMAIN);
+console.log('VITE_AUTH0_CLIENT_ID:', import.meta.env.VITE_AUTH0_CLIENT_ID);
+console.log('VITE_AUTH0_AUDIENCE:', import.meta.env.VITE_AUTH0_AUDIENCE);
 
 createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <Auth0Provider
-            domain={import.meta.env.VITE_AUTH0_DOMAIN ?? "hellooouauauaua"}
-            clientId={import.meta.env.VITE_AUTH0_CLIENT_ID ?? ""}
-            authorizationParams={{
-                redirect_uri: window.location.origin,
-                audience: import.meta.env.VITE_AUTH0_AUDIENCE ,
-            }}
-        >
-            <PaginationProvider>
-                <SnackbarProvider>
-                    <App/>
-                </SnackbarProvider>
-            </PaginationProvider>
-        </Auth0Provider>
-    </React.StrictMode>,
-)
+  <React.StrictMode>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN ?? "hellooouauauaua"}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID ?? ""}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+      }}
+    >
+      <PaginationProvider>
+        <SnackbarProvider>
+          <App />
+        </SnackbarProvider>
+      </PaginationProvider>
+    </Auth0Provider>
+  </React.StrictMode>,
+);
