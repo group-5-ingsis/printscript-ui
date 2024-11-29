037ef9b2007e904ae30852975e5adccc02b90b11
husseymarcos <mhussey@mail.austral.edu.ar>
2024-11-29 11:50:58 -0300

    fix constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
-export const FRONTEND_URL = process?.env?.FRONTEND_URL ?? "https://printscript-group5.duckdns.org/"
-export const BACKEND_URL = process?.env?.BACKEND_URL ?? "snippet"
-export const AUTH0_USERNAME = process?.env?.AUTH0_USERNAME ?? "hello"
-export const AUTH0_PASSWORD = process?.env?.AUTH0_PASSWORD ?? "Hola123!"
+export const FRONTEND_URL = "https://printscript-group5.duckdns.org/"
+export const BACKEND_URL = "snippets"
+export const AUTH0_USERNAME = "cypresstests"
+export const AUTH0_PASSWORD = "Hola123!"
 export const AUTH0_DOMAIN = "https://dev-baomkzt76ougszgg.us.auth0.com"
 
